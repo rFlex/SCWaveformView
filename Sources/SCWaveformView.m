a86f6dfb9ee00e2aae472628d7fec0c17bc49bc8
Matija Kovacic <alh84001.hr@hotmail.com>
2016-09-20 14:45:31 +0200

    Alternate iOS10 fix
    This avoids any potential redecleration issues.

--- Sources/SCWaveformView.m
+++ Sources/SCWaveformView.m
@@ -12,11 +12,14 @@
 #define noiseFloor (-50.0)
 
 #if !defined __IPHONE_10_0 || __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_10_0
-@protocol CALayerDelegate
+@protocol SCLayerDelegate
+@end
+#else
+@protocol SCLayerDelegate<CALayerDelegate>
 @end
 #endif
 
-@interface SCWaveformLayerDelegate : NSObject<CALayerDelegate>
+@interface SCWaveformLayerDelegate : NSObject<SCLayerDelegate>
 
 @end
 
